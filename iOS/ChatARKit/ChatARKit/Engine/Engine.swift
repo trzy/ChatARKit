fd593050a95bd80f4bd9bcbcb8756ec4af906d7e
Bart Trzynadlowski <bart.trzy@gmail.com>
2022-12-21 11:53:57 -0800

    Bug fix in distance()

--- iOS/ChatARKit/ChatARKit/Engine/Engine.swift
+++ iOS/ChatARKit/ChatARKit/Engine/Engine.swift
@@ -20,7 +20,7 @@ public class Engine: NSObject, ARSCNViewDelegate, ARSessionDelegate {
     private var _jsEntities: [JSValue] = []
     private var _planes: [Plane] = []
     private var _manuallyPlacedEntities: [Entity] = []
-    
+
     /// This should be called in a view controller's viewDidLoad()
     public init(sceneView: ARSCNView) {
         _sceneView = sceneView
@@ -28,12 +28,12 @@ public class Engine: NSObject, ARSCNViewDelegate, ARSessionDelegate {
         _sceneView.delegate = self
         _sceneView.showsStatistics = true
         setupJavaScriptEnvironment()
-        
+
         // Create a cube entity
         //let cube = createEntity(type: "Cube")
         //_jsContext.setObject(cube, forKeyedSubscript: "cube" as NSString)
     }
-    
+
     /// Start the engine, which starts ARKit
     public func start() {
         let configuration = ARWorldTrackingConfiguration()
@@ -41,11 +41,11 @@ public class Engine: NSObject, ARSCNViewDelegate, ARSessionDelegate {
         _sceneView.session.delegate = self
         _sceneView.session.run(configuration)
     }
-    
+
     public func pause() {
         _sceneView.session.pause()
     }
-    
+
     /// Wraps the user prompt in more context about the system to help ChatGPT generate usable code.
     public func augmentPrompt(prompt: String) -> String {
         return """
@@ -71,11 +71,11 @@ The code must obey the following constraints:
 - Does not call any functions besides those given above and those defined by the base language spec.
 """
     }
-    
+
     public func runCode(code: String) {
         _jsContext.evaluateScript(code)
     }
-    
+
     /// Debug function that simply creates a Sketchfab object using the given query to find a model
     public func placeSketchfabEntity(query: String) {
         let entity = SketchfabEntity(name: query, scene: _sceneView.scene)
@@ -91,71 +91,71 @@ The code must obey the following constraints:
         entity.node.simdWorldPosition = position
         _manuallyPlacedEntities.append(entity)
     }
-    
+
     private func onUpdateFrame(deltaTime: TimeInterval, frame: ARFrame) {
         // Update global variables
         updateGlobalVariables(frame: frame)
-        
+
         // Update entities using internal update method
         assert(_entities.count == _jsEntities.count)
         for i in 0..<_entities.count {
             _entities[i].onUpdateFrame(deltaTime: deltaTime, object: _jsEntities[i])
         }
     }
-    
+
     // MARK: - JavaScript
-    
+
     private func setupJavaScriptEnvironment() {
         // Define print() function for logging
         let printFn: @convention(block) (String) -> Void = { message in print(message) }
         _jsContext.setObject(printFn, forKeyedSubscript: "print" as NSString)
-        
+
         // Define function to get planes
         let getPlanesFn: @convention(block) () -> [Plane] = { return self.getPlanes() }
         _jsContext.setObject(getPlanesFn, forKeyedSubscript: "getPlanes" as NSString)
-        
+
         // Define function to get nearest plane to user
         let getNearestPlaneFn: @convention(block) () -> Plane? = { return self.getNearestPlane() }
         _jsContext.setObject(getNearestPlaneFn, forKeyedSubscript: "getNearestPlane" as NSString)
-        
+
         // Define function to get ground plane
         let getGroundPlaneFn: @convention(block) () -> Plane? = { return self.getGroundPlane() }
         _jsContext.setObject(getGroundPlaneFn, forKeyedSubscript: "getGroundPlane" as NSString)
-        
+
         // Define function to create entities
         let createEntityFn: @convention(block) (String) -> JSValue = { type in return self.createEntity(type: type) }
         _jsContext.setObject(createEntityFn, forKeyedSubscript: "createEntity" as NSString)
-        
+
         // ChatGPT often insists on using a distance() function even when we tell it not to
         let distanceFn: @convention(block) ([NSNumber], [NSNumber]) -> Float = { return self.distance($0, $1) }
         _jsContext.setObject(distanceFn, forKeyedSubscript: "distance" as NSString)
     }
-    
+
     private func updateGlobalVariables(frame: ARFrame) {
         _cameraTransform = frame.camera.transform
         let cameraPosition = frame.camera.transform.position
         _jsContext.setObject([ cameraPosition.x, cameraPosition.y, cameraPosition.z ], forKeyedSubscript: "cameraPosition" as NSString)
     }
-    
+
     private func getPlanes() -> [Plane] {
         return _planes
     }
-    
+
     private func getNearestPlane() -> Plane? {
         if let cameraPosition = _cameraTransform?.position {
             return _planes.sorted(by: { Vector3.distance($0.centerPosition, cameraPosition) < Vector3.distance($1.centerPosition, cameraPosition) }).first
         }
         return nil
     }
-    
+
     private func getGroundPlane() -> Plane? {
         return _planes.sorted(by: { $0.centerPosition.y < $1.centerPosition.y }).first
     }
-    
+
     private func createEntity(type: String) -> JSValue {
         print("[Engine] createEntity called: \(type)")
         let type = (type.count > 0 && type != "undefined") ? type.lowercased() : "cube"
-        
+
         // Check to see if an existing entity exists (because follow-up prompts will often reproduce the
         // original code) and return that if so
         for i in 0..<_entities.count {
@@ -163,7 +163,7 @@ The code must obey the following constraints:
                 return _jsEntities[i]
             }
         }
-        
+
         // Create a new one
         var entity: Entity?
         switch type {
@@ -177,9 +177,9 @@ The code must obey the following constraints:
         _jsEntities.append(value)
         return value
     }
-    
+
     private func distance(_ a: [NSNumber], _ b: [NSNumber]) -> Float {
-        if a.count != 3 && b.count != 3 {
+        if a.count != 3 || b.count != 3 {
             return 0
         }
         let dx = a[0].floatValue - b[0].floatValue
@@ -187,36 +187,36 @@ The code must obey the following constraints:
         let dz = a[2].floatValue - b[2].floatValue
         return sqrt(dx * dx + dy * dy + dz * dz)
     }
-    
+
     // MARK: - ARSessionDelegate
-    
+
     public func session(_ session: ARSession, didUpdate frame: ARFrame) {
         let lastFrameTime = _lastFrameTime ?? frame.timestamp
         onUpdateFrame(deltaTime: frame.timestamp - lastFrameTime, frame: frame)
         _lastFrameTime = frame.timestamp
     }
-    
+
     public func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
     }
-    
+
     public func sessionWasInterrupted(_ session: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
     }
-    
+
     public func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
     }
-    
+
     // MARK: - ARSCNViewDelegate
-    
+
     public func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
         let plane = Plane(anchor: planeAnchor, in: _sceneView)
         node.addChildNode(plane)
         _planes.append(plane)
     }
-    
+
     public func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         // Only update anchors and nodes set up by `renderer(_:didAdd:for:)`
         guard let planeAnchor = anchor as? ARPlaneAnchor, let plane = node.childNodes.first as? Plane else {
@@ -224,7 +224,7 @@ The code must obey the following constraints:
         }
         plane.update(planeAnchor: planeAnchor)
     }
-    
+
     public func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
         _planes.removeAll { $0 == node }
     }
